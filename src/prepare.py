001f536f2117a6c3112f88f3decf149a6d8c9326
Maciej Brzozowski <maciek.brzozowski149@gmail.com>
2024-04-08 14:26:15 +0200

    changed paths

--- src/prepare.py
+++ src/prepare.py
@@ -1,11 +1,11 @@
 import numpy as np
 from split import split2, split3
 
-data = np.loadtxt('data/dane.data')
+data = np.loadtxt('../data/dane.data')
 training2,test2=split2(data)
-np.savetxt("data/training2.data",training2)
-np.savetxt("data/test2.data",test2)
+np.savetxt("../data/training2.data",training2)
+np.savetxt("../data/test2.data",test2)
 training3,validation3,test3=split3(data)
-np.savetxt("data/training3.data",training3)
-np.savetxt("data/validation3.data",validation3)
-np.savetxt("data/test3.data",test3)
+np.savetxt("../data/training3.data",training3)
+np.savetxt("../data/validation3.data",validation3)
+np.savetxt("../data/test3.data",test3)
